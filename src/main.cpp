e4aeaab7418a415fd23f37d0c3302f104ae572c4
John Boiles <johnaboiles@gmail.com>
2018-02-18 23:46:46 -0800

    Downloading and displaying a jpg

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
+#define FS_NO_GLOBALS
+#include <FS.h>
+// JPEG decoder library
+#include <JPEGDecoder.h>
 #include <ESP8266WiFi.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
 #include "fast_st3375.h"
 #include <ESP8266HTTPClient.h>
-#include <FS.h>
 #include <ESP8266FtpServer.h>
 
 // GPIO 0, 2 or 15 control the boot-mode of the ESP, maybe scary to use those
@@ -76,7 +79,7 @@ void downloadFile(const char *URL) {
     if(httpCode == HTTP_CODE_OK) {
 
       // open file for writing
-      File f = SPIFFS.open("/out.png", "w");
+      fs::File f = SPIFFS.open("/out.jpg", "w");
       if (!f) {
           Serial.println("file open failed");
       }
@@ -111,6 +114,103 @@ void downloadFile(const char *URL) {
     }
   }
   http.end();
+}
+
+//====================================================================================
+//   Decode and paint onto the TFT screen
+//====================================================================================
+void jpegRender(int xpos, int ypos) {
+
+  // retrieve infomration about the image
+  uint16_t  *pImg;
+  uint16_t mcu_w = JpegDec.MCUWidth;
+  uint16_t mcu_h = JpegDec.MCUHeight;
+  uint32_t max_x = JpegDec.width;
+  uint32_t max_y = JpegDec.height;
+
+  // Jpeg images are draw as a set of image block (tiles) called Minimum Coding Units (MCUs)
+  // Typically these MCUs are 16x16 pixel blocks
+  // Determine the width and height of the right and bottom edge image blocks
+  uint32_t min_w = min(mcu_w, max_x % mcu_w);
+  uint32_t min_h = min(mcu_h, max_y % mcu_h);
+
+  // save the current image block size
+  uint32_t win_w = mcu_w;
+  uint32_t win_h = mcu_h;
+
+  // record the current time so we can measure how long it takes to draw an image
+  uint32_t drawTime = millis();
+
+  // save the coordinate of the right and bottom edges to assist image cropping
+  // to the screen size
+  max_x += xpos;
+  max_y += ypos;
+
+  // read each MCU block until there are no more
+  while ( JpegDec.read()) {
+
+    // save a pointer to the image block
+    pImg = JpegDec.pImage;
+
+    // calculate where the image block should be drawn on the screen
+    int mcu_x = JpegDec.MCUx * mcu_w + xpos;
+    int mcu_y = JpegDec.MCUy * mcu_h + ypos;
+
+    // check if the image block size needs to be changed for the right and bottom edges
+    if (mcu_x + mcu_w <= max_x) win_w = mcu_w;
+    else win_w = min_w;
+    if (mcu_y + mcu_h <= max_y) win_h = mcu_h;
+    else win_h = min_h;
+
+    // calculate how many pixels must be drawn
+    uint32_t mcu_pixels = win_w * win_h;
+
+    // draw image MCU block only if it will fit on the screen
+    if ( ( mcu_x + win_w) <= tft.width() && ( mcu_y + win_h) <= tft.height())
+	{
+      // Now set a MCU bounding window on the TFT to push pixels into (x, y, x + width - 1, y + height - 1)
+      tft.setAddrWindow(mcu_x, mcu_y, mcu_x + win_w - 1, mcu_y + win_h - 1);
+
+      // Write all MCU pixels to the TFT window
+      while (mcu_pixels--) tft.pushColor(*pImg++); // Send MCU buffer to TFT 16 bits at a time
+    }
+
+    // Stop drawing blocks if the bottom of the screen has been reached,
+    // the abort function will close the file
+    else if ( ( mcu_y + win_h) >= tft.height()) JpegDec.abort();
+
+  }
+}
+
+void drawFSJpeg(const char *filename, int xpos, int ypos) {
+
+  Serial.println("=====================================");
+  Serial.print("Drawing file: "); Serial.println(filename);
+  Serial.println("=====================================");
+
+  // Open the file (the Jpeg decoder library will close it)
+  fs::File jpgFile = SPIFFS.open( filename, "r");    // File handle reference for SPIFFS
+  //  File jpgFile = SD.open( filename, FILE_READ);  // or, file handle reference for SD library
+
+  if ( !jpgFile ) {
+    Serial.print("ERROR: File \""); Serial.print(filename); Serial.println ("\" not found!");
+    return;
+  }
+
+  // To initialise the decoder and provide the file, we can use one of the three following methods:
+  //boolean decoded = JpegDec.decodeFsFile(jpgFile); // We can pass the SPIFFS file handle to the decoder,
+  //boolean decoded = JpegDec.decodeSdFile(jpgFile); // or we can pass the SD file handle to the decoder,
+  boolean decoded = JpegDec.decodeFsFile(filename);  // or we can pass the filename (leading / distinguishes SPIFFS files)
+                                                     // The filename can be a String or character array
+  if (decoded) {
+    // print information about the image to the serial port
+    // jpegInfo();
+
+    // render the image onto the screen at given coordinates
+    jpegRender(xpos, ypos);
+  } else {
+    Serial.println("Jpeg file format not supported!");
+  }
 }
 
 void setup(void) {
@@ -159,12 +259,13 @@ void loop() {
   tft.println("Connected!");
 
 
-  delay(2000);
   // downloadFile(downloadURL.getValue());
-  downloadFile("http://image.flaticon.com/icons/png/128/148/148836.png");
+  downloadFile("http://www.gravatar.com/avatar/b26354198c6e8a01b014a81810bafe36?s=128");
+
+  drawFSJpeg("/out.jpg", 0, 0);
 
   while(true) {
-    drawacloud();
+    // drawacloud();
     yield();
     ftpSrv.handleFTP();
   }
